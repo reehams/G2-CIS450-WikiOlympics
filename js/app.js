3ba248d2457b38d346251c22d6a75adfab91e557
Reeham Sedky <reehams@seas.upenn.edu>
2017-12-06 20:22:49 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -105,7 +105,28 @@ app.controller('topAthleteController', function($scope,$http,$window,$route) {
     }; 
 }); 
 
+//Controller for Athlete info
+app.controller('athleteInfoController', function($scope, $http, $window, $route) {
+    // Insert is the name of the button -> check the about-us.html page for the button and how I registered its name
+    $scope.ATHLETE = function() {
+        // checking out the get request in router.js where I query the db
 
+        var request = $http.get('/athlete/' + $scope.firstname + '/' + $scope.surname);
+        console.log("get data");
+        request.success(function(data) {
+            console.log("SENDING DATA");
+            // check out the about-us html where I display the data
+            console.log(data);
+
+            if (data.message == undefined) {
+                $scope.data = data;
+            } else {
+                $window.alert(data.message);
+                $route.reload();
+            }
+        });
+    };
+});
 
 /*
     As a general rule: have 1 controller per button or drop-down menu
